d33a4d18d4f931373312fd72516c9d4b452edc71
Jacob Cuomo <jacobcuomodev@gmail.com>
2024-04-08 23:55:43 -0400

    Update providers.js

--- src/app/providers.js
+++ src/app/providers.js
@@ -1,12 +1,16 @@
 'use client'
 
-const config = {
-    initialColorMode: 'dark', // 'dark' | 'light'
-    useSystemColorMode: false,
-}
-
+// theme.js or wherever you define your theme
+import { extendTheme } from "@chakra-ui/react";
 import { ChakraProvider } from '@chakra-ui/react'
 
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config });
+
 export function Providers({ children }) {
-  return <ChakraProvider config={config}>{children}</ChakraProvider>
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>
 }
