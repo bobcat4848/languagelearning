3b0205f3e8ce6b3284d09912604ff0b6cc53a5ed
Jacob Cuomo <jacobcuomodev@gmail.com>
2024-04-09 00:04:13 -0400

    final fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { AuthProvider } from '@/authProviders';
 import { Providers } from './providers'
+import { ColorModeScript } from '@chakra-ui/react'
+import theme from './theme';
 
 export const metadata = {
   title: "Japanese Learning",
@@ -9,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
       <html lang='en'>
-          <body><AuthProvider><Providers>{children}</Providers></AuthProvider></body>
+          <body>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <AuthProvider><Providers>{children}</Providers></AuthProvider></body>
       </html>
     )
   }
--- src/app/providers.js
+++ src/app/providers.js
@@ -1,15 +1,7 @@
 'use client'
 
-// theme.js or wherever you define your theme
-import { extendTheme } from "@chakra-ui/react";
 import { ChakraProvider } from '@chakra-ui/react'
-
-const config = {
-  initialColorMode: 'dark',
-  useSystemColorMode: false,
-};
-
-const theme = extendTheme({ config });
+import theme from "./theme";
 
 export function Providers({ children }) {
   return <ChakraProvider theme={theme}>{children}</ChakraProvider>
